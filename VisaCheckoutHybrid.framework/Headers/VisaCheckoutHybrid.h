8dd76ea2643ff4f8562df2d6882b380c45d5d068
Jenkins 2 <dev@sentia.com.au>
2020-09-14 15:05:37 +1000

    Removed UIWebview

--- VisaCheckoutHybrid.framework/Headers/VisaCheckoutHybrid.h
+++
@@ -1,8 +0,0 @@
-/**
- Copyright © 2017 Visa. All rights reserved.
- */
-
-#import <UIKit/UIKit.h>
-
-#import "VisaCheckoutHybrid/VisaCheckoutPlugin.h"
-#import "VisaCheckoutHybrid/VisaMessageHandler.h"
